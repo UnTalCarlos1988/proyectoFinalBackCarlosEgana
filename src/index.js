8cd96b4e221dba8c55617aa089652abaff289840
carlos egana <carlos.egana.gutierrez@gmail.com>
2024-03-25 23:55:24 -0300

    1er commit

--- src/index.js
+++ src/index.js
@@ -1,74 +1,65 @@
-import express from 'express';
-import cartRouter from './routes/cartRouter.js';
-import productsRouter from './routes/productsRouter.js';
-import upload from './config/multer.js';
-import { Server } from 'socket.io';
-import { engine } from 'express-handlebars';
-import { __dirname } from './path.js';
-import { Product } from './config/Product.js';
+import express from 'express'
+import cartRouter from './routes/cartRouter.js'
+import productsRouter from './routes/productsRouter.js'
+import userRouter from './routes/userRouter.js'
+import chatRouter from './routes/chatRouter.js'
+import upload from './config/multer.js'
+import mongoose from 'mongoose'
+import messageModel from './models/messages.js'
+import { Server } from 'socket.io'
+import { engine } from 'express-handlebars'
+import { __dirname } from './path.js'
 
-// Configuraciones o declaraciones
-const app = express();
-const PORT = 8000;
+//Configuraciones o declaraciones
+const app = express()
+const PORT = 8000
 
-// Server
+//Server
 const server = app.listen(PORT, () => {
-    console.log(`Server on port ${PORT}`);
-});
+    console.log(`Server on port ${PORT}`)
+})
 
-const io = new Server(server);
+const io = new Server(server)
 
-// Middlewares
-app.use(express.json());
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
-app.set('views', __dirname + '/views');
+//Connection DB
+mongoose.connect("mongodb+srv://franciscopugh01:password@cluster0.uggkmbj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    .then(() => console.log("DB is connected"))
+    .catch(e => console.log(e))
 
-// Lista de productos
-const productos = [];
+//Middlewares
+app.use(express.json())
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
+app.set('views', __dirname + '/views')
 
-// Socket.IO
 io.on('connection', (socket) => {
-    console.log("Conexion con Socket.io");
+    console.log("Conexion con Socket.io")
 
-    socket.on('nuevoProducto', (productoData) => {
-        const nuevoProducto = new Product(
-            productoData.title,
-            productoData.description,
-            productoData.price,
-            productoData.stock,
-            productoData.code
-        );
-
-        productos.push(nuevoProducto);
-
-        io.emit('actualizarProductos', productos);
-    });
-
-    socket.on('eliminarProducto', (productoId) => {
-        const indice = productos.findIndex(p => p.id === productoId);
-        if (indice !== -1) {
-            productos.splice(indice, 1);
-            io.emit('actualizarProductos', productos);
+    socket.on('mensaje', async (mensaje) => {
+        try {
+            await messageModel.create(mensaje)
+            const mensajes = await messageModel.find()
+            io.emit('mensajeLogs', mensajes)
+        } catch (e) {
+            io.emit('mensajeLogs', e)
         }
-    });
-});
 
-// Routes
-app.use('/static', express.static(__dirname + '/public'));
-app.use('/api/products', productsRouter, express.static(__dirname + '/public'));
-app.use('/api/cart', cartRouter);
+    })
+
+})
+
+//Routes
+app.use('/public', express.static(__dirname + '/public'))
+app.use('/api/products', productsRouter, express.static(__dirname + '/public'))
+app.use('/api/cart', cartRouter)
+app.use('/api/chat', chatRouter, express.static(__dirname + '/public'))
+app.use('/api/users', userRouter)
 
 app.post('/upload', upload.single('product'), (req, res) => {
     try {
-        console.log(req.file);
-        res.status(200).send("Imagen cargada correctamente");
+        console.log(req.file)
+        res.status(200).send("Imagen cargada correctamente")
     } catch (e) {
-        res.status(500).send("Error al cargar imagen");
+        res.status(500).send("Error al cargar imagen")
     }
-});
-
-
-
-
-
+})
--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,13 +1,34 @@
 const socket = io()
 
-socket.emit('movimiento', "Ca7")
+const chatBox = document.getElementById('chatBox')
+const messageLogs = document.getElementById('messageLogs')
+let user
 
-socket.emit('rendirse', "Me he rendido")
-
-socket.on('mensaje-jugador', info => {
-    console.log(info)
+Swal.fire({
+    title: "Inicio de Sesion",
+    input: "text",
+    text: "Por favor ingrese su nombre de usuario para continuar",
+    inputValidator: (valor) => {
+        return !valor && 'Ingrese un valor valido'
+    },
+    allowOutsideClick: false
+}).then(resultado => {
+    user = resultado.value
+    console.log(user)
 })
 
-socket.on('rendicion', info => {
-    console.log(info)
+chatBox.addEventListener('change', (e) => {
+
+    if (chatBox.value.trim().length > 0) {
+        socket.emit('mensaje', { usuario: user, mensaje: chatBox.value, hora: new Date().toLocaleString() })
+        chatBox.value = ""
+    }
+
+})
+
+socket.on('mensajeLogs', info => {
+    messageLogs.innerHTML = ""
+    info.forEach(mensaje => {
+        messageLogs.innerHTML += `<p>${mensaje.hora}hs. Usuario ${mensaje.usuario} dice: ${mensaje.mensaje}</p>`
+    })
 })
